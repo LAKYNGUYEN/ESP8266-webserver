6a04ddb7e8d38d0f223bdef28216636ef172127e
LAKYNGUYEN <21001201.nguyen@student.iuh.edu.vn>
2024-04-16 09:37:04 +0700

    Update-webserver

--- include/MAIN_PAGE.h
+++ include/MAIN_PAGE.h
@@ -3,50 +3,45 @@
 
 
 const char *MAIN_PAGE = R"html(
-<!doctypehtml><html lang=en><meta charset=UTF-8><meta content="IE=edge"http-equiv=X-UA-Compatible><meta content="width=device-width,initial-scale=1"name=viewport><title>LED controller</title><style>html{font-family:Arial,Helvetica,sans-serif;margin:0 auto;text-align:center}a{text-decoration:none;color:#ede6e6}.container{background-color:#3b757b;width:300px;height:auto;border-radius:10px;margin:auto;padding:10px}body{background-color:#efefef;margin-top:50px}h1{color:#ede7e7;margin:50px 30px}h3{color:#f3f3f3;margin-bottom:50px}.container.button{display:block;width:100px;text-align:center;border:none;color:#d7e6e4;padding:50px 20px;text-decoration:none;font-size:25px;margin:0 auto 35px;cursor:pointer;border-radius:10px}.container .button{background-color:rgba(255 255 255 / .3);padding:5px 10px;color:#fff;border-radius:5px}.container .button:active{background-color:rgba(255 255 255 / .5)}.container .button-on{background-color:#00f}p{font-size:15px;color:#98c29d;margin:5px}</style><div class=container><h1>ESP8266 Wedsever</h1><h3>Control Led</h3><p>Led Status</p><button class=button>ON</button></div><div class=container style=margin-top:40px><h3>Nhiệt độ: <span class=temp-text></span></h3><h3>Độ ẩm: <span class=humi-text></span></h3></div><script>const ws = new WebSocket(`ws://${window.location.host}:81`)
-    const $btn = document.querySelector('.button')
+<!doctypehtml><html lang=en><meta charset=UTF-8><meta content="width=device-width,initial-scale=1"name=viewport><title>ESP8266</title><link href=https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css rel=stylesheet crossorigin=anonymous integrity=sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH><link href=https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css rel=stylesheet><style>.main-content{margin-top:50px;width:100%;max-width:450px}.form-check-control{width:50px!important;height:25px!important}.card{border-width:0!important;box-shadow:0 0 10px rgba(0,0,0,.1);border-radius:12px;margin-bottom:40px}.card-circle{display:flex;justify-content:center;align-items:center;width:150px;height:150px;border-radius:200px}</style><div class="container-fluid main-content"><div class=card><div class="d-flex align-items-center card-body"><div><span class=fs-4>LED Builtin</span></div><div class="form-check form-switch ms-auto"><input class="form-check-control form-check-input"id=led-btn role=switch type=checkbox></div></div></div><div class="container mt-5"><div class="d-flex justify-content-center"><div class="card-circle bg-danger-subtle"><span class=fs-3><i class="far fa-thermometer-half"></i> <span class=temp-text></span></span></div><div class="card-circle bg-info-subtle ms-5"><span class=fs-3><i class="far fa-humidity"></i> <span class=humi-text></span></span></div></div></div></div><script crossorigin=anonymous integrity=sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz src=https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js></script><script>const ws = new WebSocket(`ws://${window.location.host}:81`)
+const $btn = document.querySelector('#led-btn')
 
-    // HTTP GET request
-    function req(ep, cb) {
-        fetch(ep)
-            .then((res) => res.text())
-            .then((data) => {
-                if (cb)
-                    cb(data)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
+// HTTP GET request
+function req(ep, cb) {
+    fetch(ep)
+        .then((res) => res.text())
+        .then((data) => {
+            if (cb)
+                cb(data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+function updateButton(state) {
+    $btn.checked = state === '0' ? true : false
+}
+
+// Kiem tra thai hien tai cua led
+req('/getstatus', updateButton)
+
+// Khi ma nhan nut
+
+$btn.addEventListener('click', (e) => {
+    req('/toggle', updateButton)
+})</script><script>ws.addEventListener('message', (e) => {
+    const msg = e.data
+    if (msg.startsWith('LED_STATE')) {
+        updateButton(msg.split(':')[1])
     }
 
-    function updateButton(state) {
-        state === '0'
-            ? $btn.classList.add('button-on')
-            : $btn.classList.remove('button-on') 
-        $btn.innerHTML = state === '0' ? 'ON' : 'OFF'
+    if (msg.startsWith('READ_DHT')) {
+        const [temp, humi] = msg.split(':')[1].split('-')
+        document.querySelector('.temp-text').innerHTML = temp + '°C'
+        document.querySelector('.humi-text').innerText = humi + '%'
     }
-
-    // Kiem tra thai hien tai cua led
-    req('/getstatus', updateButton)
-
-    // Khi ma nhan nut
-
-    $btn.addEventListener('click', (e) => {
-        req('/toggle', updateButton)
-    })</script><script>ws.addEventListener('message', (e) => {
-        const msg = e.data
-        console.log(msg)
-
-        if (msg.startsWith('LED_STATE')) {
-            updateButton(msg.split(':')[1])
-        }
-
-        if (msg.startsWith('READ_DHT')) {
-            const [temp, humi] = msg.split(':')[1].split('-')
-            document.querySelector('.temp-text').innerHTML = temp + '°C'
-            document.querySelector('.humi-text').innerText = humi + '%'
-        }
-    })</script>
+})</script>
 )html";
 
 
